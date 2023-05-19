349591f7782925bc8526276de7b6fba32bb41851
ncc-mockups <133851508+ncc-mockups@users.noreply.github.com>
2023-05-19 15:05:21 +0100

    Add files via upload

---
+++ assets/js/chart.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+	
+	// Bar Chart
+
+	var barChartData = {
+		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+		datasets: [{
+			label: 'Low Risks',
+			backgroundColor: 'rgba(0, 255, 0)',
+			borderColor: 'rgba(0, 158, 251, 1)',
+			borderWidth: 1,
+			data: [35, 59, 80, 81, 56, 55, 40]
+		}, {
+			label: 'Medium Risks',
+			backgroundColor: 'rgba(255, 188, 53, 0.5)',
+			borderColor: 'rgba(255, 188, 53, 1)',
+			borderWidth: 1,
+			data: [28, 48, 40, 19, 86, 27, 90]
+		},
+		{
+			label: 'High Risks',
+			backgroundColor: 'rgba(255, 0, 0)',
+			borderColor: 'rgba(255, 188, 53, 1)',
+			borderWidth: 1,
+			data: [18, 38, 30, 15, 46, 27, 50]
+		}]
+	};
+
+	var ctx = document.getElementById('bargraph').getContext('2d');
+	window.myBar = new Chart(ctx, {
+		type: 'bar',
+		data: barChartData,
+		options: {
+			responsive: true,
+			legend: {
+				display: false,
+			}
+		}
+	});
+
+	// Line Chart
+
+	var lineChartData = {
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+		datasets: [{
+			label: "Low Risks",
+			backgroundColor: "rgba(0, 255, 0)",
+			data: [100, 70, 20, 100, 120, 50, 70, 50, 50, 100, 50, 90]
+		}, {
+		label: "Medium Risks",
+		backgroundColor: "rgba(255, 188, 53, 0.5)",
+		fill: true,
+		data: [28, 48, 40, 19, 86, 27, 20, 90, 50, 20, 90, 20]
+		},
+		{
+			label: "High Risks",
+			backgroundColor: "rgba(255, 0, 0)",
+			fill: true,
+			data: [28, 38, 40, 19, 46, 27, 20, 30, 10, 20, 10, 20]
+			}]
+	};
+	
+	var linectx = document.getElementById('linegraph').getContext('2d');
+	window.myLine = new Chart(linectx, {
+		type: 'line',
+		data: lineChartData,
+		options: {
+			responsive: true,
+			legend: {
+				display: false,
+			},
+			tooltips: {
+				mode: 'index',
+				intersect: false,
+			}
+		}
+	});
+	
+	// Bar Chart 2
+	
+    barChart();
+    
+    $(window).resize(function(){
+        barChart();
+    });
+    
+    function barChart(){
+        $('.bar-chart').find('.item-progress').each(function(){
+            var itemProgress = $(this),
+            itemProgressWidth = $(this).parent().width() * ($(this).data('percent') / 100);
+            itemProgress.css('width', itemProgressWidth);
+        });
+    };
+});
